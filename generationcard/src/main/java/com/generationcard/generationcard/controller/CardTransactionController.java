4e5c1c22150d23b351237de1fd2ee151895828f5
OumaimaRTAYLI <oumaima.rtayli@gmail.com>
2024-10-31 16:25:32 +0100

    link with monolith - tests to be done yet

--- generationcard/src/main/java/com/generationcard/generationcard/controller/CardTransactionController.java
+++ generationcard/src/main/java/com/generationcard/generationcard/controller/CardTransactionController.java
@@ -1,5 +1,7 @@
 package com.generationcard.generationcard.controller;
 
+import com.generationcard.generationcard.model.ImageData;
+import com.generationcard.generationcard.model.TextData;
 import com.generationcard.generationcard.model.Transaction;
 import com.generationcard.generationcard.producer.TransactionMessageProducer;
 import com.generationcard.generationcard.service.CardTransactionService;
@@ -24,14 +26,14 @@ public class CardTransactionController {
 
 
     @PostMapping("/receive-text")
-    public ResponseEntity<String> receiveTextData(@RequestBody String textData) {
-        cardTransactionService.saveTextData(textData.Text());
+    public ResponseEntity<String> receiveTextData(@RequestBody TextData textData) {
+        cardTransactionService.saveTextData(textData);
         return ResponseEntity.ok("Text data received successfully");
     }
 
     @PostMapping("/receive-image")
-    public ResponseEntity<String> receiveImageData(@RequestBody String imageData) {
-        cardTransactionService.saveImageData(imageData.getImageUrl());
+    public ResponseEntity<String> receiveImageData(@RequestBody ImageData imageData) {
+        cardTransactionService.saveImageData(imageData);
         return ResponseEntity.ok("Image data received successfully");
     }
 
