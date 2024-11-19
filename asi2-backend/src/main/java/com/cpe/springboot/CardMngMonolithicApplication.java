fbe3ae652ab9e46c2a7cd5177acfbbadbaffa5e6
raghad <raghad.alhomsi2225@gmail.com>
2024-11-19 23:25:10 +0100

    DiscoveryClient

--- asi2-backend/src/main/java/com/cpe/springboot/CardMngMonolithicApplication.java
+++ asi2-backend/src/main/java/com/cpe/springboot/CardMngMonolithicApplication.java
@@ -2,6 +2,7 @@ package com.cpe.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.jms.annotation.EnableJms;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
@@ -9,6 +10,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 
 @EnableJms
 @SpringBootApplication
+@EnableDiscoveryClient
 @OpenAPIDefinition(info = @Info(title = "Card Market Rest Api", version = "1.0", description = "Information about the Card Market APi and how to interact with"))
 // doc here localhost:8080/swagger-ui.html
 public class CardMngMonolithicApplication {
